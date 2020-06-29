5bf6b0f30461bcf89143a22d09b543bd05dbbcee
sarasempere <semperesara@gmail.com>
2020-06-29 21:50:54 +0000

    modificando index.js

--- src/js/index.js
+++ src/js/index.js
@@ -7,22 +7,32 @@ import "../style/index.scss";
 
 window.onload = function() {
   console.log("Hello Rigo from the console!");
-  let who = [" Sergio", " Javi"];
-  let action = [" dormía", " estaba"];
-  let what = [" en casa", " en la playa", " poniéndose la mascarilla"];
-  let when = [" a la hora de clase"];
+  let who = ["Sergio", "Javi", "Dani", "Sara"];
+  let action = [
+    "dormía",
+    "estaba",
+    "montaba en triciclo",
+    "perreaba",
+    "tomaba unas cañitas"
+  ];
+  let what = ["en casa", "en la playa", "en el bar"];
+  let when = ["a la hora de clase", "en la ITV", "mientras paseaba al gato"];
 
   return console.log(createExcuse(who, action, what, when));
 };
 
-function createExcuse(a) {
-  let excuse = +randomText(a);
+function createExcuse(...a) {
+  let excuse = "";
+  for (let i = 0; i < a.length; i++) {
+    let arr = a[i];
+    let palabra = randomText(arr);
+    excuse = excuse + " " + palabra;
+  }
   return excuse;
 }
 
 function randomText(a) {
-  let arrLength = a.length;
-  let randomNumber = Math.floor(Math.random() * arrLength);
+  let randomNumber = Math.floor(Math.random() * a.length);
   let palabra = a[randomNumber];
   return palabra;
 }
